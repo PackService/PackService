7393c1f15f42313d4fefd6a65fbda0a93f58fb76
Leebumju <dlqkawns@naver.com>
2022-12-26 18:08:21 +0900

    ,,

--- PackService/View/MemberShipAgreementView.swift
+++ PackService/View/MemberShipAgreementView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MemberShipAgreementView: View {
     @Binding var firstNaviLinkActive: Bool
     var agreementName = ["이용약관", "개인정보 처리방침"]
-    var agreementContent = ["aslfjalefjaoseifjapoeifjapo efjaosifjapweoifj asdoifj apweoifj aspdofijwpe ofijqwdpofij qwepoifj apsoijfeapsoeifj apoeisjf aposeifj apsoeifj apsoeifjaopsiej fpaosiej fpaoisejf poasiejdfpoasiej paosie fjpoaisje fopiasje fawefoaiofij qwepoifj apsoijfeapsoeifj apoeisjf aposeifj apsoeifj apsoeifjaopsiej fpaosiej fpaoisejf poasiejdfpoasiej paosie fjpoaisje fopiasje fawe aposeifj apsoeifj apsoeifjaopsiej f paosie fjpoaisje fopiasje fawefoai" , "개인정보처리방침 뭐시기저시기 ㅁ애랴멎대랴ㅓ메ㅐ쟈ㅓ레ㅐ먀젇레ㅐ머ㅑ애ㅔ랴멎대ㅔ랴ㅓㅁ제ㅐ댜러메재댜러메재댜러메재댜러메ㅐㄴ야ㅓㄹ매댜ㅓ래ㅑㅓㄷ매랴ㅐ먀젇래먀젇레매ㅑㅈ더레매쟈더레매쟏러ㅔ매쟏러ㅔ맺댜러ㅔ매쟈더램젇"]
+    var agreementContent = ["aslfjalefjaoseifjapoeifjapo iej f paosie fjpoaisje fopiasje fawefoai" , "개인정보처리방침 뭐시기저시기 ㅁ쟏러ㅔ맺댜러ㅔ매쟈더램젇"]
     var body: some View {
         List(0...1, id: \.self) { idx in
             DisclosureGroup {
