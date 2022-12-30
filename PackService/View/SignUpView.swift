53a5b199999845419854293ec7f2cc45a032cfe7
Leebumju <dlqkawns@naver.com>
2022-12-30 14:38:02 +0900

    회원가입 작업 중

--- PackService/View/SignUpView.swift
+++ PackService/View/SignUpView.swift
@@ -14,26 +14,43 @@ struct SignUpView: View {
     @State var passwordInput: String = ""
     @State var passwordCheckInput: String = ""
     var body: some View {
-        VStack{
-            TextField("이메일", text: $emailInput).keyboardType(.emailAddress).autocapitalization(.none)
-            SecureField("비밀번호", text: $passwordInput).keyboardType(.default)
-            SecureField("비밀번호 확인", text: $passwordCheckInput).keyboardType(.default)
-            Button {
-                viewModel.registerUser(email: emailInput, password: passwordInput)
-            } label: {
-                Text("등록")
+        HStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("계정을")
+                    .padding(.leading, 10)
+                    .font(.custom("Pretendard-Bold", size: 30))
+                Text("만들어주세요")
+                    .padding(.leading, 10)
+                    .font(.custom("Pretendard-Bold", size: 30))
+                Spacer()
+                TextField("이메일", text: $emailInput)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
+                SecureField("비밀번호", text: $passwordInput).keyboardType(.default)
+                SecureField("비밀번호 확인", text: $passwordCheckInput).keyboardType(.default)
             }
-            Text("회원가입")
-            Button(action: {
-                firstNaviLinkActive = false
-            }, label: {
-                Text("Main으로 돌아가기")
-                    .foregroundColor(Color.white)
-                    .frame(width: 100, height: 60, alignment: .center)
-                    .background(RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.purple))
-            })
+            Spacer()
         }
+//        VStack{
+//            TextField("이메일", text: $emailInput).keyboardType(.emailAddress).autocapitalization(.none)
+//            SecureField("비밀번호", text: $passwordInput).keyboardType(.default)
+//            SecureField("비밀번호 확인", text: $passwordCheckInput).keyboardType(.default)
+//            Button {
+//                viewModel.registerUser(email: emailInput, password: passwordInput)
+//            } label: {
+//                Text("등록")
+//            }
+//            Text("회원가입")
+//            Button(action: {
+//                firstNaviLinkActive = false
+//            }, label: {
+//                Text("Main으로 돌아가기")
+//                    .foregroundColor(Color.white)
+//                    .frame(width: 100, height: 60, alignment: .center)
+//                    .background(RoundedRectangle(cornerRadius: 10)
+//                        .fill(Color.purple))
+//            })
+//        }
     }
 }
 
