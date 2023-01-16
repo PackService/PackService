3f45c4ca90919cc10249106d1a24d0a22ba0bee4
Leebumju <dlqkawns@naver.com>
2023-01-16 20:01:16 +0900

    packlist button 클릭 시 색상변경 완료

--- PackService/View/PackListItemView.swift
+++ PackService/View/PackListItemView.swift
@@ -82,43 +82,45 @@ struct PackListItemView: View {
     @Binding var packInfoModels: [PackInfoModel]
     var body: some View {
         SwipeItem(content: {
-            Button(action: {
-                
-            }, label: {
-                HStack {
-                    Spacer()
-                    VStack {
-                        HStack {
-                            Image(systemName: "circle.fill")
-                                .resizable()
-                                .frame(width: 44, height: 44)
-                            VStack(alignment: .leading) {
-                                Text(packInfoModel.packageName)
-                                    .font(FontManager.title3)
-                                    .padding(.bottom, 2)
-                                HStack {
-                                    Text(packInfoModel.packageNumber)
-                                        .font(FontManager.caption1)
-                                        .foregroundColor(ColorManager.foreground1)
-                                    Spacer()
-                                    Text(packInfoModel.packageArrvieTime)
-                                        .font(FontManager.caption1)
-                                        .foregroundColor(ColorManager.foreground1)
-                                        .padding(.trailing, 8)
-                                    Text(packInfoModel.packageState)
-                                        .font(FontManager.caption2)
-                                        .foregroundColor(ColorManager.primaryColor)
+            VStack {
+                Button(action: {
+                    deletePackInfoModel()
+                }, label: {
+                    HStack {
+                        Spacer()
+                        VStack {
+                            HStack {
+                                Image(systemName: "circle.fill")
+                                    .resizable()
+                                    .frame(width: 44, height: 44)
+                                VStack(alignment: .leading) {
+                                    Text(packInfoModel.packageName)
+                                        .font(FontManager.title3)
+                                        .padding(.bottom, 2)
+                                    HStack {
+                                        Text(packInfoModel.packageNumber)
+                                            .font(FontManager.caption1)
+                                            .foregroundColor(ColorManager.foreground1)
+                                        Spacer()
+                                        Text(packInfoModel.packageArrvieTime)
+                                            .font(FontManager.caption1)
+                                            .foregroundColor(ColorManager.foreground1)
+                                            .padding(.trailing, 8)
+                                        Text(packInfoModel.packageState)
+                                            .font(FontManager.caption2)
+                                            .foregroundColor(ColorManager.primaryColor)
+                                    }
                                 }
+                                .padding(.leading, 16)
+                                Spacer()
                             }
-                            .padding(.leading, 16)
-                            Spacer()
+                            .padding(16)
                         }
-                        .padding(16)
+                        Spacer()
                     }
-                    Spacer()
-                }
-            })
-            .buttonStyle(CapsuleButtonStyle())
+                })
+                .buttonStyle(ListButtonStyle())
+            }
         }, right: {
             VStack {
                 Button(action: {
@@ -153,6 +155,16 @@ struct PackListItemView: View {
         }
     }
 }
+
+struct ListButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .background(configuration.isPressed ? ColorManager.primaryColor : .clear )
+            .cornerRadius(10)
+            .padding(.trailing, 20)
+    }
+}
+
 
 // 만약 화면에서는 데이터 지워지는데 백 쪽에서 안지워진다면 https://www.youtube.com/watch?v=jXVQDmeNb8A 14분 확인해보기
 
