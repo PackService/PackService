fd15686215f433df08388878325a63b8373883e5
Leebumju <dlqkawns@naver.com>
2023-01-25 14:50:37 +0900

    온보딩부터 회원가입까지 연결 작업 중

--- PackService/View/OnBoardingView.swift
+++ PackService/View/OnBoardingView.swift
@@ -14,87 +14,60 @@ struct OnBoardingView: View {
     @StateObject var kakaoAuthVM = KakaoAuthVM()
     @StateObject var appleAuthVM = AppleAuthVM()
     var body: some View {
-        
         NavigationView {
-            VStack(alignment: .center) {
-                Text("내 소중한 택배의\n우당탕탕 대모험 📦")
-                    .multilineTextAlignment(.center)
-                    .font(FontManager.title1)
-                    .lineSpacing(5)
-                
-                Spacer()
-                
-                VStack(spacing: 16) {
-//                    Button {
-//
-//                    } label: {
-//                        ThirdPartyButtonView(type: .apple)
-//                    }
-                    SignInWithAppleButton { (request) in
-                        appleAuthVM.nonce = randomNonceString()
-                        request.requestedScopes = [.email,.fullName]
-                        request.nonce = sha256(appleAuthVM.nonce)
-                    } onCompletion: { (result) in
-                        //getting error or success
-                        switch result {
-                        case .success(let user):
-                            print("apple login success")
-                            guard let credential = user.credential as? ASAuthorizationAppleIDCredential else {
-                                print("error with firebase")
-                                return
-                            }
-                            appleAuthVM.authenticate(credential: credential)
-                        case .failure(let error):
-                            print(error.localizedDescription)
+            ZStack {
+                VStack(alignment: .center) {
+                    Text("내 소중한 택배의\n우당탕탕 대모험 📦")
+                        .multilineTextAlignment(.center)
+                        .font(FontManager.title1)
+                        .lineSpacing(5)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 16) {
+                        Button {
+                            kakaoAuthVM.handleKakaoLogin()
+                        } label: {
+                            ThirdPartyButtonView(type: .kakao)
+                        }
+                        
+                        Button {
+                            signUpScreen.toggle()
+                        } label: {
+                            ThirdPartyButtonView(type: .email)
+                        }
+
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 16)
+                    
+                    HStack {
+                        Text("이미 계정이 있나요?")
+                            .foregroundColor(Color("foreground1"))
+                        
+                        NavigationLink {
+                            LoginUIView()
+                        } label: {
+                            Text("로그인")
+                                .foregroundColor(Color("primary_color"))
+                                .overlay(
+                                    Rectangle()
+                                        .frame(height: 1)
+                                        .offset(y: 2)
+                                        .foregroundColor(Color("primary_color"))
+                                    , alignment: .bottom)
                         }
                     }
-                    .cornerRadius(26)
-                    .foregroundColor(Color.white)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 52)
-                    .background(Color.black.cornerRadius(26))
-                    
-                    Button {
-                        kakaoAuthVM.handleKakaoLogin()
-                    } label: {
-                        ThirdPartyButtonView(type: .kakao)
-                    }
-                    
-                    Button {
-                        signUpScreen.toggle()
-                    } label: {
-                        ThirdPartyButtonView(type: .email)
-                    }
+                    .font(FontManager.body2)
                 }
-                .padding(.horizontal, 20)
-                .padding(.bottom, 16)
-                
-                HStack {
-                    Text("이미 계정이 있나요?")
-                        .foregroundColor(Color("foreground1"))
-                    
-                    NavigationLink {
-                        LoginUIView()
-                    } label: {
-                        Text("로그인")
-                            .foregroundColor(Color("primary_color"))
-                            .overlay(
-                                Rectangle()
-                                    .frame(height: 1)
-                                    .offset(y: 2)
-                                    .foregroundColor(Color("primary_color"))
-                                , alignment: .bottom)
-                    }
+                .padding(.vertical, 41)
+                if signUpScreen { // 회원가입 화면 이동
+                    MemberShipAgreementView(signUpScreen: $signUpScreen)
+                        .transition(.move(edge: .bottom))
+                        .animation(.spring())
                 }
-                .font(FontManager.body2)
             }
-            .padding(.vertical, 41)
         }
-//        if signUpScreen { // 회원가입 화면 이동
-//            MemberShipAgreementView(signUpScreen: $signUpScreen)
-//                .transition(.move(edge: .bottom))
-//                .animation(.spring())
-//        }
     }
 }
 
